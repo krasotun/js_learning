a28b06a149fba181a69501c66a309aa3b7a32a45
krasotun <servrostov@gmail.com>
2021-11-13 18:44:04 +0300

    add Добавил кнопку для выбора первого значения

--- yandex_practicum/intro/script.js
+++ yandex_practicum/intro/script.js
@@ -1,16 +1,26 @@
 let words = ['Первая фраза', 'Вторая фраза', 'Третья фраза', 'Четвертая фраза', 'Пятая фраза', 'Шестая фраза', 'Седьмая фраза', 'Восьмая фраза', 'Девятая фраза', 'Десятая фраза', ];
 
 
-function getRandomWord() {
-    let randomIndex = Math.floor(Math.random() * words.length);
-    let randomWord = words[randomIndex];
-    return randomWord;
+function getRandomWord(arr) { // для выбора рандомного значения
+    let randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
 }
 
-let redButton = document.querySelector('.btn');
-let mainText = document.querySelector('.main_text');
+function getFirstWord(arr) { // для выбора первого значения
+    return arr[0];
 
+}
+
+let redButton = document.querySelector('.red_btn');
+let blueButton = document.querySelector('.blue_btn');
+
+let mainText = document.querySelector('.main_text');
+// mainText.textContent = getRandomWord(words);
 
 redButton.addEventListener('click', function () {
-    mainText.textContent = getRandomWord();
+    mainText.textContent = getRandomWord(words);
+});
+
+blueButton.addEventListener('click', function () {
+    mainText.textContent = getFirstWord(words);
 });
