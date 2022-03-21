8b8b5eaf54c7365cd8169f6a77643694379b44be
krasotun <servrostov@gmail.com>
2022-03-22 00:10:19 +0300

    add Добавил больше ООП в интерфейсах

--- yandex_practicum/OOP/ООП в интерфейсах.js
+++ yandex_practicum/OOP/ООП в интерфейсах.js
@@ -18,25 +18,32 @@ const items = [{
 	},
 ];
 
+const popupElement = document.querySelector('.popup');
+const popupImage = document.querySelector('.popup__image');
+const popupCloseButton = document.querySelector('.popup__close');
 
 class Card {
-	constructor(title, description, price, image) {
-		this._title = title;
-		this._description = description;
-		this._price = price;
-		this._image = image;
+	constructor(data, cardSelector) {
+		this._title = data.title;
+		this._description = data.description;
+		this._price = data.price;
+		this._image = data.image;
+		this._cardSelector = cardSelector;
 	}
+
 	_getTemplate() {
 		const cardElement = document
-			.querySelector('.horizontal-card')
+			.querySelector(this._cardSelector)
 			.content
 			.querySelector('.card')
 			.cloneNode(true);
 
 		return cardElement;
 	}
+
 	generateCard() {
 		this._element = this._getTemplate();
+
 		this._element.querySelector('.card__image').style.backgroundImage = `url(${this._image})`;
 		this._element.querySelector('.card__title').textContent = this._title;
 		this._element.querySelector('.card__info').textContent = this._description;
@@ -44,15 +51,53 @@ class Card {
 
 		return this._element;
 	}
+
+	_handleOpenPopup() {
+		popupImage.src = this._image;
+		popupElement.classList.add('popup_is-opened');
+	}
+	_handleClosePopup() {
+		popupImage.src = '';
+		popupElement.classList.remove('popup_is-opened');
+
+	}
+	_setEventListeners() {
+		this._element.addEventListener('click', () => {
+			this._handleOpenPopup();
+		});
+		popupCloseButton.addEventListener('clik', () => {
+			popupCloseButton._handleClosePopup();
+		});
+	}
+
+	/* Теперь создайте метод _setEventListeners, чтобы он добавлял все обработчики в одном месте.Опишите в нём события клика по двум элементам:
+		карточке this._element;
+	иконке закрытия, которая хранится в переменной popupCloseButton.
+	Пусть клик по этим элементам вызывает соответствующие методы из класса Card.Колбэк слушателя событий— стрелочная функция.
+	В финале добавьте в тело метода generateCard вызов _setEventListeners.Тогда метод создаст карточки уже с обработчиком. */
 }
 
-items.forEach(item => {
-	const card = new Card(item.title, item.description, item.price, item.image);
-	const cardElement = card.generateCard;
+items.forEach((item) => {
+	const card = new Card(item, '.horizontal-card');
+	const cardElement = card.generateCard();
+	this._setEventListeners();
+	// Добавляем в DOM
 	document.querySelector('.card-list__items').append(cardElement);
-
 });
 
+/* Поработайте над интернет - магазином.Пусть по клику на карточку товара появляется всплывающее окно, которое уже есть в вашем DOM - дереве.Попап скрывается, если кликнуть на крестик.
+	Для этого создайте внутри класса Card два метода обработки событий: _handleOpenPopup— при клике на карточку, _handleClosePopup— при клике на крестик.
+	Мы подготовили переменные popupImage и popupElement.В них находятся элементы с изображением в окне попапа и само это окно.
+	В методе _handleOpenPopup:
+		переменной popupImage переопределите свойство src на this._image,
+		переменной popupElement методом classList.add добавьте класс popup_is - opened.
+	В методе _handleClosePopup:
+		переменной popupImage переопределите свойство src на '',
+		переменной popupElement методом classList.remove уберите класс popup_is - opened. */
+
+
+/* Подготовьте класс к увеличению объёма данных.Научите его принимать список аргументов в виде объекта.Назовите этот параметр data.
+Конструктор сделайте универсальным для разных template - элементов.Для этого добавьте в него селектор шаблона.Назовите этот параметр cardSelector.Код внутри метода forEach оставьте без изменений. */
 
 
 /* Осталось опубликовать карточки.Обойдите массив items и для каждого его элемента:
