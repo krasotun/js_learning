515b332ee92863efc8d3ef4b12c21087319d93ba
krasotun <servrostov@gmail.com>
2022-04-26 16:14:36 +0300

    add Добавил рфд Фибоначчи

--- you-dont-know-js/Fibonacchi.js
+++ you-dont-know-js/Fibonacchi.js
@@ -19,7 +19,7 @@ function recursiveFoo(num) {
 	memento[num] = recursiveFoo(num - 2) + recursiveFoo(num - 1);;
 	return memento[num];
 }
-console.log(recursiveFoo(45));
+console.log(recursiveFoo(100));
 
 function recursiveFooOld(num) {
 	if (num === 1 || num === 2) {
@@ -31,3 +31,19 @@ function recursiveFooOld(num) {
 }
 console.log(recursiveFooOld(46));
 
+
+
+function computeFibonacciElement(n) {
+	if (n <= 2) return 1;
+	const f = [0, 1, 1];
+	for (let i = 3; i <= n; i++) {
+		f[i] = f[i - 1] + f[i - 2];
+	}
+	return f[f.length - 1];
+}
+
+console.log(computeFibonacciElement(100));
+
+
+
+console.log(Math.floor(4.5));
