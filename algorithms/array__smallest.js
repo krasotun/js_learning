b6ffddc16dc22d2f89b3d74331f831105f6070ce
krasotun <servrostov@gmail.com>
2022-06-29 17:49:06 +0300

    add Добавил тесты для массивов

--- algorithms/array__smallest.js
+++ algorithms/array__smallest.js
@@ -1,5 +1,3 @@
-const array = [36542, 12313, 444, 75, 56,
-	1886, 17, 1888, 17, 270, 39, 40]
 
 // Поиск наименьшего значения
 function findSmallest(arr) {
@@ -12,18 +10,28 @@ function findSmallest(arr) {
 	return smallest
 }
 
-
-console.log('Массив из чисел', findSmallest(array));
-
-// Передали пустой массив
+// Пустой массив
 const emptyArray = [];
 console.log('Пустой  массив', findSmallest(emptyArray));
 
-// Передали массив из строк
+// Массив из строк
+const stringsArray = ['One', 'Two', "Three", "Four", 'Five']
+console.log('Массив из строк', findSmallest(stringsArray));
 
+// Массив чисел
+const digitsArray = [36542, 12313, 444, 75, 56,
+	1886, 17, 1888, 17, 270, 39, 40]
+console.log('Массив из чисел', findSmallest(digitsArray));
 
-// Передали смешанный массив (строки, цифры)
+// Cмешанный массив (строки, цифры)
+const mixedArray = [36542, 12313, 444, 75, 56,
+	1886, 17, 1888, 17, 270, 39, 40, 'One', 'Two', "Three", "Four", 'Five']
+console.log('Смешанный массив', findSmallest(mixedArray));
+
 // Передали массив из массивов
+const arrayFromArrays = [[1, 2, 3], [8, 95], [45, 32, 26], [226, 456, 7869], [21112, 1285, 778, 6], [132]]
+console.log('Массив из масивов', findSmallest(arrayFromArrays));
+
 
 
 
