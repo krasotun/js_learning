cd4b74140b78fe0628d6290a1707d895b4568189
krasotun <servrostov@gmail.com>
2022-06-30 23:42:50 +0300

    add Добавил пару задач

--- codewars/0 Base.js
+++ codewars/0 Base.js
@@ -12,8 +12,23 @@ const rps = (p1, p2) => {
 			|| (p1 === 'paper') && (p2 === 'scissors'))) {
 		return 'Player 1 won'
 	} else if
-		((p1 === 'scissors' && p2 === 'scissors')
-		|| (p1 === 'rock' && p2 === 'rock')
-		|| (p1 === 'paper') && (p2 === 'paper')) {
+		(p1 === p2) {
 	} return 'Draw'
 };
+
+
+// Реализуйте функцию, которая умножает два числа.
+// https://www.codewars.com/kata/523b66342d0c301ae400003b/train/javascript
+
+function multiply(a, b) {
+	// Складываем a+a b-1 раз
+
+	for (let i = 0; i < b - 1; i++) {
+		let c = a + a;
+		c = c + a;
+	}
+	return c
+}
+
+console.log(multiply(3, 6));
+
