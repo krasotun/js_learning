447fd2cd1641e55396594c477a0b9e23c0cbfbb7
Marat <servrostov@gmail.com>
2022-11-05 15:49:11 +0300

    add router

--- practikum_middle/router/index.js
+++ practikum_middle/router/index.js
@@ -10,9 +10,31 @@ class Block {
 	}
 }
 
-class Button extends Block {
+class Chats extends Block {
 	getContent() {
-		return 'Button';
+		return 'chats';
+	}
+
+	show() {
+		console.log('show chats');
+	}
+
+	hide() {
+		console.log('hide chats');
+	}
+}
+
+class Users extends Block {
+	getContent() {
+		return 'users';
+	}
+
+	show() {
+		console.log('show users');
+	}
+
+	hide() {
+		console.log('hide users');
 	}
 }
 
@@ -36,10 +58,15 @@ class Route {
 
 	navigate(pathname) {
 		if (this.match(pathname)) {
+			this._pathname = pathname;
+			this.render();
 		}
 	}
 
 	leave() {
+		if (this._block) {
+			this._block.hide();
+		}
 	}
 
 	match(pathname) {
@@ -47,17 +74,100 @@ class Route {
 	}
 
 	render() {
+		if (!this._block) {
+			this._block = new this._blockClass();
+			render(this._props.rootQuery, this._block);
+			return;
+		}
+
+		this._block.show();
 	}
 }
 
-const route = new Route('/buttons', Button, {
-	rootQuery: '.app',
-});
+class Router {
+	constructor(rootQuery) {
+		if (Router.__instance) {
+			return Router.__instance;
+		}
 
-route.render();
+		this.routes = [];
+		this.history = window.history;
+		this._currentRoute = null;
+		this._rootQuery = rootQuery;
 
-console.log(route._pathname, route._props); // /buttons, {rootQuery: '.app'}
+		Router.__instance = this;
+	}
 
-route.navigate('/buttons'); // show
-route.navigate('/trash'); // не будет никакого лога
-route.leave(); // hide
+	use(pathname, block) {
+		const route = new Route(pathname, block, { rootQuery: this._rootQuery });
+
+		this.routes.push(route);
+
+		return this;
+	}
+
+	start() {
+		window.onpopstate = (event => {
+			this._onRoute(event.currentTarget.location.pathname);
+		}).bind(this);
+
+		this._onRoute(window.location.pathname);
+	}
+
+	_onRoute(pathname) {
+		const route = this.getRoute(pathname);
+		if (!route) {
+			return;
+		}
+
+		if (this._currentRoute && this._currentRoute !== route) {
+			this._currentRoute.leave();
+		}
+
+		this._currentRoute = route;
+		route.render(route, pathname);
+	}
+
+	go(pathname) {
+		this.history.pushState({}, '', pathname);
+		this._onRoute(pathname);
+	}
+
+	back() {
+		this.history.back();
+	}
+
+	forward() {
+		this.history.forward();
+	}
+
+	getRoute(pathname) {
+		return this.routes.find(route => route.match(pathname));
+	}
+}
+
+// Необходимо оставить в силу особенностей тренажёра
+history.pushState({}, '', '/');
+
+const router = new Router(".app");
+
+// Можно обновиться на /user и получить сразу пользователя
+router
+	.use("/", Chats)
+	.use("/users", Users)
+	.start();
+
+// Через секунду контент изменится сам, достаточно дёрнуть переход
+setTimeout(() => {
+	router.go("/users");
+}, 1000);
+
+// А можно и назад
+setTimeout(() => {
+	router.back();
+}, 3000);
+
+// И снова вперёд
+setTimeout(() => {
+	router.forward();
+}, 5000);
