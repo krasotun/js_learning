45adca0b2566f27cd6ffac996688b38e49111112
krasotun <servrostov@gmail.com>
2022-12-24 22:20:42 +0300

    add some code

--- algorithms/arrays-cross.js
+++ algorithms/arrays-cross.js
@@ -17,3 +17,21 @@ findEqualElements([2, 2, 2, 2], [1, 2, 2, 3]) // => [2, 2]
 // Даны два отсортированных числовых массива.
 // Напишите функцию, которая находит пересечение двух массивов и возвращает его.
 // Пересечение — массив из элементов, которые встречаются в обоих массивах.
+
+// Let arr1, arr2 be the two sorted arrays of length La and Lb.
+// Let i be index into the array arr1.
+// Let j be index into the array arr2.
+// Initialize i and j to 0.
+
+// while (i < La and j < Lb) do
+
+// 	if (arr1[i] == arr2[j]) { // found a common element.
+//         print arr[i] // print it.
+//         increment i // move on.
+//         increment j
+// 	}
+// 	else if (arr1[i] > arr2[j])
+//         increment j // don't change i, move j.
+//     else
+//         increment i // don't change j, move i.
+// end while
