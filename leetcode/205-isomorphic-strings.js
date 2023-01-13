34772b447efd2c9fa3abd74a2b43167eeaa47c9c
krasotun <servrostov@gmail.com>
2023-01-13 12:25:10 +0300

    add isomorth

---
+++ leetcode/205-isomorphic-strings.js
@@ -0,0 +1,26 @@
+// 205. Isomorphic Strings
+
+// https://leetcode.com/problems/isomorphic-strings/?envType=study-plan&id=level-1
+
+// Given two strings s and t, determine if they are isomorphic.
+// Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+// All occurrences of a character must be replaced with another character
+// while preserving the order of characters.No two characters may map to the
+// same character, but a character may map to itself.
+
+
+
+// Input: s = "egg", t = "add"
+// Output: true
+// Example 2:
+
+// Input: s = "foo", t = "bar"
+// Output: false
+// Example 3:
+
+// Input: s = "paper", t = "title"
+// Output: true
+
+const isIsomorphic = (s, t) => {
+
+}
