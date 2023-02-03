a0ca535c48caeee7c0cdbe0f0bf93ad01386e21d
krasotun <servrostov@gmail.com>
2023-02-03 16:56:05 +0300

    add:pairs

--- algorithms/strings-pairs.js
+++ algorithms/strings-pairs.js
@@ -1,36 +1,36 @@
-function wordVariants(words) {
-	let wordIndex = 0;
-	let wordVariantIndex = 0;
-	return function getNextVariant() {
-		if (wordIndex === words.length) return undefined;
-		if (wordVariantIndex === words[wordIndex].length) {
-			wordIndex++;
-			wordVariantIndex = 0;
-			return getNextVariant();
-		}
-		let currentVariant = words[wordIndex][wordVariantIndex];
-		wordVariantIndex++;
-		let result = words.slice(0, wordIndex).concat([currentVariant]).concat(words.slice(wordIndex + 1)).join(" ");
-		return result;
-	}
-}
-
-
-// Внучку –> ['внучка', 'внучок', ...]
-// Машу –> ['маша', 'махать', 'машу', ...]
-// ...
-
 function allSentences(words) {
-	//
+	const indexes = words.map(() => 0);
+
+	return function () {
+		if (indexes[0] === words[0].length) {
+			return undefined;
+		};
+
+		const sentence = indexes
+			.map((caseIndex, wordIndex) => words[wordIndex][caseIndex])
+			.join(' ');
+
+		let i = indexes.length - 1;
+		while (i >= 0) {
+			indexes[i]++;
+			if (indexes[i] === words[i].length) {
+				if (i === 0) {
+					break;
+				};
+
+				indexes[i] = 0;
+				i--;
+				continue;
+			}
+			break;
+		}
+
+		return sentence;
+	}
 }
 
 const nextSentence = allSentences([
 	['внучка', 'внучок'],
-	['маша', 'махать', 'машу']
+	['маша', 'махать', 'машу',]
 	// ... тут могут быть другие слова с любым количеством своих форм
 ]);
-
-nextSentence(); // 'внучка маша'
-nextSentence(); // 'внучка махать'
-// ... и т. д. все комбинации
-nextSentence(); // undefined
