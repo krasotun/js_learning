f709b20fa24aec0cbc70248e5b262b20a6a5e904
krasotun <servrostov@gmail.com>
2023-02-15 11:39:25 +0300

    add: replace elements

--- leetcode/arrays/1299-replace-elements.js
+++ leetcode/arrays/1299-replace-elements.js
@@ -21,6 +21,15 @@
 // Explanation: There are no elements to the right of index 0.
 
 function replaceElements(arr) {
-
+	let max = arr[arr.length - 1]
+	arr[arr.length - 1] = -1;
+	for (let i = arr.length - 2; i >= 0; i--) {
+		let cur = arr[i];
+		arr[i] = max;
+		if (cur > max) {
+			max = cur;
+		}
+	}
+	return arr;
 };
-replaceElements([17, 18, 5, 4, 6, 1])
+replaceElements([17, 18, 5, 4, 6, 1]) // [18, 6, 6, 6, 1, -1]
