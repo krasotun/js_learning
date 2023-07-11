e75bd09e1646c0d62aaedd6d604bd9186ec004a2
krasotun <servrostov@gmail.com>
2023-07-11 17:18:54 +0300

    add algs from yandex

--- algorithms/two-sum.js
+++ algorithms/two-sum.js
@@ -12,15 +12,44 @@ const result = twoSums(
 fs.writeFileSync("output.txt", result);
 
 function twoSums(len, array, digit) {
-  for (let i = 0; i < len; i++) {
-    for (let j = i + 1; j < len; j++) {
-      if (array[i] + array[j] === digit) {
-        return [array[i], array[j]].join(" ");
-      }
+  const sorted = [...array.sort((a, b) => a - b)];
+
+  let left = 0;
+  let right = len - 1;
+
+  while (left < right) {
+    const sum = sorted[left] + sorted[right];
+
+    if (sum === digit) {
+      return [sorted[left], sorted[right]].join(" ");
+    }
+
+    if (sum > digit) {
+      right -= 1;
+    }
+
+    if (sum < digit) {
+      left += 1;
+    }
+
+    if (left === right) {
+      return "None";
     }
   }
-  return "None";
 }
 
-console.log(twoSums(6, [-1, -1, -9, -7, 3, -6], 2)); // -1 3
-console.log(twoSums(8, [6, 2, 8, -3, 1, 1, 6, 10], 100)); // none
+console.log(twoSums(10, [-91, -56, -44, -32, -10, 33, 55, 58, 87, 94], -88)); // none
+// console.log(twoSums(6, [-1, -1, -9, -7, 3, -6], 2)); // -1 3
+
+// function twoSums(len, array, digit) {
+//   for (let i = 0; i < len; i++) {
+//     for (let j = i + 1; j < len; j++) {
+//       if (array[i] + array[j] === digit) {
+//         return [array[i], array[j]].join(" ");
+//       }
+//     }
+//   }
+//   return "None";
+// }
+
+console.log(-56 + -32);
