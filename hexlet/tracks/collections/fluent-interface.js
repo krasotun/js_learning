180d8c12b5ceae8e61feb88537ddc94b135b28af
krasotun <servrostov@gmail.com>
2023-09-12 22:24:23 +0300

    add fluent intergace

--- hexlet/tracks/collections/fluent-interface.js
+++ hexlet/tracks/collections/fluent-interface.js
@@ -3,43 +3,36 @@ class Enumerable {
     this.collection = collection;
   }
 
-  sorterAsc(a, b) {
-    if (a === b) {
-      return 0;
-    }
-
-    if (a < b) {
-      return -1;
-    }
-
-    if (b < a) {
-      return 1;
-    }
-  }
-
-  sorterDesc(a, b) {
-    if (a === b) {
-      return 0;
-    }
-
-    if (a > b) {
-      return -1;
-    }
-
-    if (b > a) {
-      return 1;
-    }
-  }
-
   select(fn) {
     this.collection = this.collection.map(fn);
     return this;
   }
 
   orderBy(fn, direction = "asc") {
-    this.collection = this.collection.sort(
-      direction === "asc" ? this.sorterAsc : this.sorterDesc
-    );
+    if (direction !== "asc" && direction !== "desc") {
+      direction = "asc";
+    }
+
+    this.collection.sort((a, b) => {
+      const itemA = fn(a);
+      const itemB = fn(b);
+
+      if (typeof itemA === "number" && typeof itemB === "number") {
+        if (direction === "asc") {
+          return itemA - itemB;
+        } else if (direction === "desc") {
+          return itemB - itemA;
+        }
+      }
+
+      if (typeof itemA === "string" && typeof itemB === "string") {
+        if (direction === "asc") {
+          return itemA.localeCompare(itemB);
+        } else if (direction === "desc") {
+          return itemB.localeCompare(itemA);
+        }
+      }
+    });
     return this;
   }
 
@@ -58,13 +51,13 @@ export default Enumerable;
 const cars = [
   { brand: "bmw", model: "m4", year: 2013 },
   { brand: "bmw", model: "m4 gt3", year: 2020 },
-  { brand: "bmw", model: "m5", year: 2014 },
   { brand: "kia", model: "sorento", year: 2014 },
+  { brand: "bmw", model: "m5", year: 2014 },
   { brand: "kia", model: "rio", year: 2010 },
   { brand: "kia", model: "sportage", year: 2012 },
 ];
 const coll = new Enumerable(cars);
-const result = coll.orderBy((car) => car.year);
+const result = coll.orderBy((car) => car.model);
 
 // const result = coll
 //   .orderBy((car) => car.year, "desc")
