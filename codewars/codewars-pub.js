05cd8eebe3a8ef4d9282dbc8aacea724b989f814
krasotun <servrostov@gmail.com>
2023-12-12 20:41:53 +0300

    add codewars tasks

---
+++ codewars/codewars-pub.js
@@ -0,0 +1,25 @@
+// Your fellow coders have bought you several drinks tonight in the form of a string.
+//  Return a string suggesting how many glasses of water you should drink to not be hungover.
+
+// Examples
+
+// "1 beer"  -->  "1 glass of water"
+// because you drank one standard drink
+
+// "1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer"  -->  "10 glasses of water"
+// because you drank ten standard drinks
+
+function hydrate(s) {
+  const count = s
+    .split(" ")
+    .map((el) => Number(el))
+    .filter((el) => !isNaN(el))
+    .reduce((acc, cur) => {
+      acc = acc + cur;
+      return acc;
+    }, 0);
+
+  return count === 1 ? `1 glass of water` : `${count} glasses of water`;
+}
+
+hydrate("1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer");
